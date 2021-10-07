51930158cfa436d8e3f1e74554d178f6b7d91499
syb999 <13062678135@163.com>
2021-10-07 21:47:36 +0800

    add luci-app-adbrun

---
+++ package/network/services/luci-app-adbrun/files/adbrun/adbcommand.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+spath="/usr/adbrun/"
+sectionname=$(echo $0 | cut -d '_' -f 2)
+adbclient=$(uci get adbrun.$sectionname.adbiplist)
+adbcommand=$(uci get adbrun.$sectionname.adbcommandlist)
+
+adb connect ${adbclient}:5555
+screensize=$(adb -s ${adbclient}:5555 shell wm size | cut -d ':' -f 2 | sed -e "s/ //g;s/\n//g;s/\r//g")
+if [ ${screensize} == "720x1280" ];then
+	sizepath="720x1280"
+elif  [ ${screensize} == "1080x2244" ];then
+	sizepath="1080x2244"
+elif  [ ${screensize} == "1080x1920" ];then
+	sizepath="1080x1920"
+fi
+
+case $adbcommand in
+	turn-offon-the-screen) adbcd="shell input keyevent 26"
+	;;
+	turn-on-the-screen) adbcd="shell input keyevent 224"
+	;;
+	playstop) adbcd="shell input keyevent 85"
+	;;
+	playnext) adbcd="shell input keyevent 87"
+	;;
+	playprevious) adbcd="shell input keyevent 88"
+	;;
+	resume-playback) adbcd="shell input keyevent 126"
+	;;
+	pause-playback) adbcd="shell input keyevent 127"
+	;;
+	mute) adbcd="shell input keyevent 164"
+	;;
+	runcamera) adbcd="shell am start -n com.android.camera/.Camera"
+	;;
+	photograph) adbcd="shell input keyevent 27"
+	;;
+	appactivity) adbcd="shell dumpsys activity activities | grep -i run"
+	;;
+	runxmlylite) adbcd="shell am start -n com.ximalaya.ting.lite/com.ximalaya.ting.android.host.activity.MainActivity"
+	;;
+	runfqxs) adbcd="shell am start -n com.dragon.read/.pages.splash.SplashActivity"
+	;;
+	runwechat) adbcd="shell am start -n com.tencent.mm/.ui.LauncherUI"
+	;;
+	runqq) adbcd="shell am start -n com.tencent.mobileqq/.activity.SplashActivity"
+	;;
+	runtaobao) adbcd="shell am start -n  com.taobao.taobao/com.taobao.tao.TBMainActivity"
+	;;
+	runtaobaolite) adbcd="shell am start -n  com.taobao.litetao/com.taobao.ltao.maintab.MainFrameActivity"
+	;;
+	rundiantao) adbcd="shell am start -n com.taobao.live/.home.activity.TaoLiveHomeActivity"
+	;;
+	runjdlite) adbcd="shell am start -n com.jd.jdlite/.MainActivity"
+	;;
+	pyxmlylite) adbcd="scripts"
+		adbsh="pyxmlylite"
+	;;
+	readbook) adbcd="scripts"
+		adbsh="readbook"
+	;;
+	autodiantao) adbcd="scripts"
+		adbsh="diantao"
+	;;
+	autojdlite) adbcd="scripts"
+		adbsh="jdlite"
+	;;
+	none) adbcd=""
+	;;
+esac
+
+if [ $adbcd == "scripts" ];then
+	if [ ${adbsh} ==  "pyxmlylite" ];then
+		cp ${spath}${sizepath}/${adbsh} /tmp/${sectionname}_py
+		python3 /tmp/${sectionname}_py
+	elif  [ ${adbsh} == "readbook" ];then
+		cp ${spath}${sizepath}/${adbsh} /tmp/${sectionname}_sh
+		sh /tmp/${sectionname}_sh
+	elif  [ ${adbsh} == "diantao" ];then
+		cp ${spath}${sizepath}/${adbsh} /tmp/${sectionname}_sh
+		sh /tmp/${sectionname}_sh
+	elif  [ ${adbsh} == "jdlite" ];then
+		cp ${spath}${sizepath}/${adbsh} /tmp/${sectionname}_sh
+		sh /tmp/${sectionname}_sh
+	fi
+else
+	adb -s ${adbclient}:5555 ${adbcd}
+fi
+
---
+++ package/network/services/luci-app-adbrun/luasrc/model/cbi/adbrun.lua
@@ -0,0 +1,83 @@
+m = Map("adbrun", translate("ADB server"))
+
+s = m:section(TypedSection, "adbrun", "", translate("Assistant for automatic control android devices."))
+
+s:tab("adb_set", translate("Basic setting"))
+
+s.anonymous = false
+s.addremove = true
+
+adbiplist=s:taboption("adb_set",Value, "adbiplist", translate("IP address")) 
+adbiplist.rmempty = true
+adbiplist.datatype = "ipaddr"
+luci.sys.net.ipv4_hints(function(ip, name)
+	adbiplist:value(ip, "%s (%s)" %{ ip, name })
+end)
+
+adbcommandlist = s:taboption("adb_set", ListValue, "adbcommandlist", translate("Command list"), translate("adbrun command list"))
+adbcommandlist.placeholder = "none"
+adbcommandlist:value("none", translate("none"))
+adbcommandlist:value("turn-offon-the-screen", translate("Turn off/on the screen"))
+adbcommandlist:value("turn-on-the-screen", translate("Turn on screen"))
+adbcommandlist:value("playstop", translate("Play or stop"))
+adbcommandlist:value("playnext", translate("Play the next"))
+adbcommandlist:value("playprevious", translate("Play the previous"))
+adbcommandlist:value("resume-playback", translate("Resume playback"))
+adbcommandlist:value("pause-playback", translate("Pause playback"))
+adbcommandlist:value("mute", translate("Mute on/off"))
+adbcommandlist:value("runcamera", translate("Run camera"))
+adbcommandlist:value("photograph", translate("Take a picture"))
+adbcommandlist:value("appactivity", translate("Get running APP"))
+adbcommandlist:value("runwechat", translate("Run Wechat"))
+adbcommandlist:value("runqq", translate("Run QQ"))
+adbcommandlist:value("runtaobao", translate("Run taobao"))
+adbcommandlist:value("runtaobaolite", translate("Run taobao lite version"))
+adbcommandlist:value("rundiantao", translate("Run taobao live"))
+adbcommandlist:value("runjdlite", translate("Run JD lite version"))
+adbcommandlist:value("runfqxs", translate("Run fanqie xiaoshuo"))
+adbcommandlist:value("runxmlylite", translate("Run ximalaya lite version"))
+adbcommandlist:value("pyxmlylite", translate("Automatically get gold coins from ximalaya lite version"))
+adbcommandlist:value("readbook", translate("Automatically read book"))
+adbcommandlist:value("autodiantao", translate("Automatically taobao live"))
+adbcommandlist:value("autojdlite", translate("Automatically jdlite"))
+adbcommandlist.default     = "none"
+adbcommandlist.rempty      = false
+
+
+s:tab("adb_action", translate("Action"))
+
+adbconnect=s:taboption("adb_action",Button,"adbconnect",translate("Connect client"))
+adbconnect.rmempty = true
+adbconnect.inputstyle = "apply"
+function adbconnect.write(self, section)
+	local adbconnectip = luci.util.exec("adb connect $(uci get adbrun."..section..".adbiplist)")
+	--testconnect = luci.util.exec("adb devices | grep -i "..section)
+	--if testconnect == "" then
+		--luci.util.exec("logger Connection failed: Please connect the computer with USB cable. ")
+		--luci.util.exec("logger Run: adb tcpip 5555 ")
+	--else
+		--luci.util.exec("logger Successful: "..adbconnectip)
+	--end
+end
+
+adbplay=s:taboption("adb_action",Button, "adbplay", translate("Play")) 
+adbplay.rmempty = true
+adbplay.inputstyle = "apply"
+function adbplay.write(self, section)
+	luci.util.exec("cp /usr/adbrun/adbcommand.sh /tmp/adb_" ..section.. "_.sh")
+	luci.util.exec("/tmp/adb_" ..section.. "_.sh")
+end
+
+adbstop=s:taboption("adb_action",Button, "adbstop", translate("Stop loop script")) 
+adbstop.rmempty = true
+adbstop.inputstyle = "apply"
+function adbstop.write(self, section)
+	luci.util.exec("kill $(ps | grep " ..section.. " | grep -v grep | head -n 1 | cut -d 'r' -f 1) > /dev/null 2>&1")
+	luci.util.exec("kill $(ps | grep " ..section.. " | grep -v grep | head -n 1 | cut -d 'r' -f 1) > /dev/null 2>&1")
+	luci.util.exec("kill $(ps | grep " ..section.. " | grep -v grep | head -n 1 | cut -d ' ' -f 1) > /dev/null 2>&1")
+	luci.util.exec("kill $(ps | grep " ..section.. " | grep -v grep | head -n 1 | cut -d ' ' -f 1) > /dev/null 2>&1")
+
+end
+
+return m
+
