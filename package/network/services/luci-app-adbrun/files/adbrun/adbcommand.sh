6bc27172afb7c21b7ab862571ccca72d4e604591
syb999 <13062678135@163.com>
2021-10-20 21:37:50 +0800

    luci-app-adbrun: bump to 2021-10-20

--- package/network/services/luci-app-adbrun/files/adbrun/adbcommand.sh
+++ package/network/services/luci-app-adbrun/files/adbrun/adbcommand.sh
@@ -74,6 +74,12 @@ case $adbcommand in
 	autojdlite) adbcd="scripts"
 		adbsh="jdlite"
 	;;
+	11diantao) adbcd="scripts"
+		adbsh="11diantao"
+	;;
+	11taobaozc) adbcd="scripts"
+		adbsh="11taobaozc"
+	;;
 	none) adbcd=""
 	;;
 esac
@@ -175,6 +181,44 @@ if [ $adbcd == "scripts" ];then
 			sh /tmp/${sectionname}_sh
 		elif  [ ${screensize} == "1080x2280" ];then
 			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=530/;s/dosedystart=/ystart=1560/;s/dosedbasex=/basex=950/;s/dosedbasey=/basey=835/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		fi
+	elif  [ ${adbsh} == "11diantao" ];then
+		echo "shuang11"
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=310/;s/dosedystart=/ystart=710/;s/dosedbasex=/basex=600/;s/dosedbasey=/basey=500/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=518/;s/dosedystart=/ystart=1390/;s/dosedbasex=/basex=900/;s/dosedbasey=/basey=1705/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=518/;s/dosedystart=/ystart=1190/;s/dosedbasex=/basex=900/;s/dosedbasey=/basey=1505/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=518/;s/dosedystart=/ystart=1390/;s/dosedbasex=/basex=900/;s/dosedbasey=/basey=1705/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		fi
+	elif  [ ${adbsh} == "11taobaozc" ];then
+		echo "shuang11"
+		if [ ${screensize} == "720x1280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=320/;s/dosedystart=/ystart=1065/;s/dosedbasex=/basex=360/;s/dosedbasey=/basey=500/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x2244" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=360/;s/dosedystart=/ystart=1560/;s/dosedbasex=/basex=550/;s/dosedbasey=/basey=770/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x1920" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=360/;s/dosedystart=/ystart=1360/;s/dosedbasex=/basex=550/;s/dosedbasey=/basey=570/' > /tmp/${sectionname}_sh
+			chmod +x /tmp/${sectionname}_sh
+			sh /tmp/${sectionname}_sh
+		elif  [ ${screensize} == "1080x2280" ];then
+			cat ${spath}${adbsh} | sed 's/dosedxstart=/xstart=360/;s/dosedystart=/ystart=1560/;s/dosedbasex=/basex=550/;s/dosedbasey=/basey=770/' > /tmp/${sectionname}_sh
 			chmod +x /tmp/${sectionname}_sh
 			sh /tmp/${sectionname}_sh
 		fi
