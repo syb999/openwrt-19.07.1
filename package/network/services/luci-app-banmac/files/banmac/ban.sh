c1186149255bb0113f85783f938103f951924e98
syb999 <13062678135@163.com>
2021-11-16 00:03:49 +0800

    luci-app-banmac: bump to 2021-11-16

--- package/network/services/luci-app-banmac/files/banmac/ban.sh
+++ package/network/services/luci-app-banmac/files/banmac/ban.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-getcmac=$(uci get banmac.@banlist[0].banlist_mac)
-
+sectionname=$(echo $0 | cut -d '_' -f 2 | sed 's/^OO!%!OO//')
+getcmac=$(uci get banmac.$sectionname.banlist_mac | tr 'A-Z' 'a-z')
 iptables -I FORWARD -m mac --mac-source $getcmac -j DROP
 
+for i in $(seq 0 1)
+do
+	for x in $(iw wlan${i} station dump | grep -i station | cut -d ' ' -f 2)
+	do
+		if [ $x = $getcmac ]; then
+			iw dev wlan${i} station del $x
+		fi
+	done
+done
