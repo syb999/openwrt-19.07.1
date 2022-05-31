5ef4f1ed6bfb6c073e705b27444a309e475103bc
syb999 <13062678135@163.com>
2022-05-31 22:51:12 +0800

    luci-app-adbrun: add adb status htm

--- package/network/services/luci-app-adbrun/luasrc/controller/adbrun.lua
+++ package/network/services/luci-app-adbrun/luasrc/controller/adbrun.lua
@@ -7,4 +7,46 @@ function index()
 
 	local page = entry({"admin", "services", "adbrun"}, cbi("adbrun"), _("ADBrun"),3)
 	page.dependent = true
+
+	entry({"admin", "services", "adbrun", "status"}, call("xact_status")).leaf = true
 end
+
+function xact_status()
+	local json = require "luci.jsonc"
+	luci.http.prepare_content("application/json")
+
+	local adblist = io.popen("adb devices | sed '1d;$d' 2>/dev/null")
+	if adblist then
+		infolist = { }
+		local num = 0
+		local deviceip, port, apk, rapk, runapk
+		while true do
+			local ln = adblist:read("*l")
+			if not ln then
+				break
+			elseif ln:match("(%S-):(%d+).-") then
+				deviceip, port = ln:match("(%S-):(%d+).-")
+
+				if num and deviceip and port then
+					apk = io.popen("adb -s " .. deviceip .. ":" .. port .." shell dumpsys activity activities | grep -i run | grep -e 'SplashActivity' -e 'MainActivity' -e 'AudioPlayActivity' -e 'NewMapActivity' -e 'NewMainActivity' -e 'LauncherUI' -e 'MainFrameActivity' -e 'AlipayLogin' -e 'InnerUCMobile' -e 'MediaActivity' | grep -v miui. | grep -v android.systemui | grep -v recent | awk '{print $5}' | cut -d '/' -f 1 | head -n 1 2>/dev/null")
+					rapk = apk:read("*a")
+					if rapk then
+						runapk = rapk
+					end
+					num = num + 1
+				end
+			end
+
+			infolist[#infolist+1] = {
+				num = num,
+				deviceip = deviceip,
+				port = port,
+				apk = runapk
+			}
+		end
+	end
+	adblist:close()
+	
+	luci.http.write_json(infolist);
+end
+
